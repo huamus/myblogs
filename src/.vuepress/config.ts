dc53aa4bda7003cba5de8c97fa3f2bf2b7ace723
huamus <huamus>
2023-06-24 07:12:36 +0800

    first commit

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,10 +2,11 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
+  // 这里要修改成自己的github仓库名字，注意前后要加 /
   base: "/myblogs/",
 
   lang: "zh-CN",
-  title: "博客演示",
+  title: "huamus",
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
