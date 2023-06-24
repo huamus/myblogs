c8b84170eb84563522c178ac638276a57f90fc45
huamus <huamus>
2023-06-24 19:36:51 +0800

    修改导航栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -5,6 +5,30 @@ export default sidebar({
     // 博客主页
     // "",
     {
+      text: "图解网络",
+      icon: "laptop-code",
+      prefix: "/computerNetworks/",
+      children: "structure",
+    },
+    {
+      text: "图解操作系统",
+      icon: "laptop-code",
+      prefix: "/operatingSystem/",
+      children: "structure",
+    },
+    {
+      text: "MySQL",
+      icon: "laptop-code",
+      prefix: "/MySQL/",
+      children: "structure",
+    },
+    {
+      text: "Redis",
+      icon: "laptop-code",
+      prefix: "/Redis/",
+      children: "structure",
+    },
+    {
       text: "如何使用",
       icon: "laptop-code",
       prefix: "demo/",
