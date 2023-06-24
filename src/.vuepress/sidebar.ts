56067ba87c47998748907e585510c4daeb2de8e9
huamus <huamus>
2023-06-24 20:10:05 +0800

    修改导航栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,39 +1,16 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/xiaolinCoding": [
-    // 博客主页
-    // "",
+  "/": [
+    "",
     {
-      text: "图解网络",
-      icon: "laptop-code",
-      prefix: "/computerNetworks/",
-      children: "structure",
-    },
-    {
-      text: "图解操作系统",
-      icon: "laptop-code",
-      prefix: "/operatingSystem/",
-      children: "structure",
-    },
-    {
-      text: "MySQL",
-      icon: "laptop-code",
-      prefix: "/MySQL/",
-      children: "structure",
-    },
-    {
-      text: "Redis",
-      icon: "laptop-code",
-      prefix: "/Redis/",
-      children: "structure",
-    },
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
+      text: "知识库",
+      icon: "lightbulb",
+      prefix: "knowledgeBase/",
+      children: [
+        "xiaolinCoding/",
+        "JavaGuide/"
+      ],
     },
     {
       text: "文章",
@@ -44,4 +21,45 @@ export default sidebar({
     "intro",
     "slides",
   ],
+
+  "/knowledge/": "structure",
+  "/book/": "structure",
+  // "/": [
+  //   // 博客主页
+  //   // "",
+  //   {
+  //     text: "图解网络",
+  //     icon: "laptop-code",
+  //     prefix: "/computerNetworks/",
+  //     children: "structure",
+  //   },
+  //   {
+  //     text: "图解操作系统",
+  //     icon: "laptop-code",
+  //     prefix: "/operatingSystem/",
+  //     children: "structure",
+  //   },
+  //   {
+  //     text: "MySQL",
+  //     icon: "laptop-code",
+  //     prefix: "/MySQL/",
+  //     children: "structure",
+  //   },
+    
+  //   {
+  //     text: "如何使用",
+  //     icon: "laptop-code",
+  //     prefix: "demo/",
+  //     link: "demo/",
+  //     children: "structure",
+  //   },
+  //   {
+  //     text: "文章",
+  //     icon: "book",
+  //     prefix: "posts/",
+  //     children: "structure",
+  //   },
+  //   "intro",
+  //   "slides",
+  // ],
 });
